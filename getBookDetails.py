e59c2ad12a365d374964f3cd30eefa4429f6ef12
Kevin Bichoupan <kevin.bichoupan@gmail.com>
2020-12-14 22:15:01 -0500

    Added web scraping for Book details and created selenium function to search for books of the same authoer.  TO DO: webscrape books of the same author from the link

--- getBookDetails.py
+++ getBookDetails.py
@@ -3,7 +3,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
+
+
+options = Options()
+options.add_argument('--headless')
 
 
 
@@ -11,10 +16,12 @@ class book():
 	def __init__(self, isbn):
 		self.isbn = isbn
 		self.getgoodreadsURL()
+		self.getGoodreadsBookDetails()
+		self.getBooksByAuthor()
 
 	def getgoodreadsURL(self):
 		chromeDriverPath = '/Users/kevinbichoupan/drivers/chromedriver'
-		driver = webdriver.Chrome(chromeDriverPath)
+		driver = webdriver.Chrome(chromeDriverPath, chrome_options=options)
 		driver.get('https://www.goodreads.com/')
 	
 		driver.find_element_by_name("query").click()
@@ -24,23 +31,31 @@ class book():
 		self.goodreadsURL  = driver.current_url
 		driver.quit()
 
-		#htmlRaw = urlopen(isbnSearchUrl)
-		#htmlSoup = soup(htmlRaw.read(),"html.parser")
-		#htmlRaw.close()
-
-		#print(htmlSoup)
-	
 	def getGoodreadsBookDetails(self):
 		htmlRaw = urlopen(self.goodreadsURL)
-		htmlSoup = soup(htmlRaw.read()."html.parser")
+		htmlSoup = soup(htmlRaw.read(), "html.parser")
 		htmlRaw.close()
+		metaContainer = htmlSoup.find(id = 'metacol', class_= 'last col')
+		
+		self.title = metaContainer.find(id = 'bookTitle').text.replace('\n','').strip()
+		self.author = metaContainer.find(class_='authorName').text
+		self.description  = metaContainer.find(id = 'descriptionContainer').text
 
-
+	def getBooksByAuthor(self):
+		chromeDriverPath = '/Users/kevinbichoupan/drivers/chromedriver'
+		driver = webdriver.Chrome(chromeDriverPath, chrome_options=options)
+		driver.get('https://www.goodreads.com/')
+	
+		driver.find_element_by_name("query").click()
+		driver.find_element_by_name("query").clear()
+		driver.find_element_by_name("query").send_keys(self.author)
+		driver.find_element_by_name("query").send_keys(Keys.ENTER)
+		self.booksByAuthorURL  = driver.current_url
+		driver.quit()
 
 if __name__ == "__main__":
 	x = book('9780451191137')
-	print(x.goodreadsURL)
-
+	print(x.booksByAuthorURL)
 
 
 
